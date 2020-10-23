77c11a63b2e87edf91ad15cbb9e5d2e739a49e70
yue-cherry-ying <cherryying@uchicago.edu>
2020-10-23 19:33:15 +0000

    finished with aries

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -10,4 +10,16 @@ class NumbersController < ApplicationController
 
     render({ :template => "lottery_stuff/woohoo.html.erb"})
   end 
+
+  def unlucky
+    @number = Array.new
+
+    5.times do
+      unluck = rand(1...100)
+
+      @number.push(unluck)
+    end 
+
+  render({ :template => "lottery_stuff/ohno.html.erb"})
+  end
 end
